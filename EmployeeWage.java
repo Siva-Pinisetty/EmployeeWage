0ac230c9d12bb0f20e76620f24cb7b2ff21c0b07
balu-pinisetty <balupinisetty@gmail.com>
2021-06-01 23:44:51 +0530

    Calcltaingg wage for a month

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,22 +4,27 @@ public class EmployeeWage {
 public static void main(String[] args) {
 	System.out.println("Welocome to Employee Wage Computation Problem");
 	Random random = new Random();
-	int Ran = random.nextInt(3);
 	int empPerHr=20;
 	int empHr = 0;
-	switch (Ran) {
-	case 2:
-		System.out.println("Employee is Full Present");
-		empHr=8;
-		break;
-	case 1:
-		System.out.println("Employee is Part-time Present");
-		empHr=4;
-		break;
-	case 0:
-		System.out.println("Employee is Absent");
+	int month = 20;
+	int empWage = 0;
+	for (int i = 0; i < month; i++) {
+		int Ran = random.nextInt(3);
+		switch (Ran) {
+		case 2:
+			System.out.println("Employee is Full Present");
+			empHr=8;
+			break;
+		case 1:
+			System.out.println("Employee is Part-time Present");
+			empHr=4;
+			break;
+		case 0:
+			System.out.println("Employee is Absent");
+		}
+		int Salary=empPerHr*empHr;
+		empWage=empWage+Salary;
 	}
-	int Salary=empPerHr*empHr;
-	System.out.println("His Salary is "+Salary);
+	System.out.println("Employee Salary for month is "+empWage);
 }
 }
