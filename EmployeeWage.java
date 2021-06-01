756c133b93d2b3c949c5eab385a5938286075baa
balu-pinisetty <balupinisetty@gmail.com>
2021-06-01 21:54:07 +0530

    Checking for the Employee Attendance

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,7 +1,17 @@
 package employeewage;
-
+import java.util.Random;
 public class EmployeeWage {
-	public static void main(String[] args) {
-		System.out.println("Welocome to Employee Wage Computation Problem");
+public static void main(String[] args) {
+	System.out.println("Welocome to Employee Wage Computation Problem");
+	Random random = new Random();
+	int Ran = random.nextInt(2);
+	if (Ran == 1)
+	{
+		System.out.println("Employee is Present");
+	}	
+	else
+	{
+		System.out.println("Employee is Absent");
 	}
+}
 }
