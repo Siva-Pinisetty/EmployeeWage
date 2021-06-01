6f59d7b7774eaffe2a2efdf83ae4f5f5ee64ec6a
balu-pinisetty <balupinisetty@gmail.com>
2021-06-01 22:44:22 +0530

    Calculating Employee wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -5,13 +5,17 @@ public static void main(String[] args) {
 	System.out.println("Welocome to Employee Wage Computation Problem");
 	Random random = new Random();
 	int Ran = random.nextInt(2);
+	int empPerHr=20;
 	if (Ran == 1)
 	{
 		System.out.println("Employee is Present");
+		int empFullHr=8;
+		int Salary=empPerHr*empFullHr;
+		System.out.println("His Salary is "+Salary);
 	}	
 	else
 	{
-		System.out.println("Employee is Absent");
+		System.out.println("Employee is Absent\nHis Salary is 0");
 	}
 }
 }
