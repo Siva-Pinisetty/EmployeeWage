59b3906f0933c174f6f59757ad9020288983bffa
balu-pinisetty <balupinisetty@gmail.com>
2021-06-01 23:27:11 +0530

    Changing into Switch case

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -7,18 +7,16 @@ public static void main(String[] args) {
 	int Ran = random.nextInt(3);
 	int empPerHr=20;
 	int empHr = 0;
-	if (Ran == 2)
-	{
+	switch (Ran) {
+	case 2:
 		System.out.println("Employee is Full Present");
 		empHr=8;
-	}	
-	else if (Ran == 1)
-	{
+		break;
+	case 1:
 		System.out.println("Employee is Part-time Present");
 		empHr=4;
-	}
-	else
-	{
+		break;
+	case 0:
 		System.out.println("Employee is Absent");
 	}
 	int Salary=empPerHr*empHr;
